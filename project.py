49274b0548f87e9fff6603a249d1706970431c73
kl3tka <89717410+kl3tka@users.noreply.github.com>
2024-04-01 14:25:36 -0300

    Update project.py

--- project.py
+++ project.py
@@ -1,7 +1,4 @@
-import os, random, string
+import secrets
 
-length = 13
-chars = string.ascii_letters + string.digits + '!@#$%^&*()'
-random.seed = (os.urandom(1024))
-
-print ''.join(random.choice(chars) for i in range(length))
+password_length = 13
+print(secrets.token_urlsafe(password_length))
